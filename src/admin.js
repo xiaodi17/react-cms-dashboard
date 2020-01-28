46f67a76a4a2d3e8b6eee6a92eafb5e9a1e7d118
David <xiaodi.david@gmail.com>
2020-01-29 00:17:14 +1000

    main page structure

--- src/admin.js
+++ src/admin.js
@@ -2,16 +2,23 @@ import React from 'react'
 import { Row, Col } from 'antd'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import NavLeft from './components/NavLeft'
+import './style/common.less'
 
 export default class Admin extends React.Component {
   render() {
     return (
-      <Row>
-        <Col span="3">Left</Col>
-        <Col span="21">
-          <Header></Header>
-          <Row></Row>
-          <Footer></Footer>
+      <Row className="container">
+        <Col span="4" className="nav-left">
+          <NavLeft />
+        </Col>
+        <Col span="20" className="main">
+          <Header />
+          <Row className="content">
+            Content
+            {/* {this.props.children} */}
+          </Row>
+          <Footer />
         </Col>
       </Row>
     )
---
+++ src/components/NavLeft/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default class NavLeft extends React.Component {
+  render() {
+    return <div>NavLeft</div>
+  }
+}
