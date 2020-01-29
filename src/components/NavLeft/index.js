e53c0661f77e73fa8aa7806b7935cf7bc7f68df3
David <xiaodi.david@gmail.com>
2020-01-29 21:53:15 +1000

    Side Menu -wip

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -1,7 +1,37 @@
 import React from 'react'
-
+import MenuConfig from './../../config/menuConfig'
+import { Menu, Icon } from 'antd'
+import './index.less'
+const SubMenu = Menu.SubMenu
 export default class NavLeft extends React.Component {
   render() {
-    return <div>NavLeft</div>
+    return (
+      <div>
+        <div className="logo">
+          <img src="/assets/logo-ant.svg" alt=""></img>
+          <h1>Imooc MS</h1>
+        </div>
+        <Menu>
+          <SubMenu
+            key="sub1"
+            title={
+              <span>
+                <Icon type="mail" />
+                <span>Navigation One</span>
+              </span>
+            }
+          >
+            <Menu.ItemGroup title="Item 1">
+              <Menu.Item key="1">Option 1</Menu.Item>
+              <Menu.Item key="2">Option 2</Menu.Item>
+            </Menu.ItemGroup>
+            <Menu.ItemGroup title="Iteom 2">
+              <Menu.Item key="3">Option 3</Menu.Item>
+              <Menu.Item key="4">Option 4</Menu.Item>
+            </Menu.ItemGroup>
+          </SubMenu>
+        </Menu>
+      </div>
+    )
   }
 }
---
+++ src/config/menuConfig.js
@@ -0,0 +1,125 @@
+const menuList = [
+  {
+    title: 'Home',
+    key: '/home'
+  },
+  {
+    title: 'UI',
+    key: '/ui',
+    children: [
+      {
+        title: 'Buttons',
+        key: '/ui/buttons'
+      },
+      {
+        title: 'Modals',
+        key: '/ui/modals'
+      },
+      {
+        title: 'Loading',
+        key: '/ui/loadings'
+      },
+      {
+        title: 'Notification',
+        key: '/ui/notification'
+      },
+      {
+        title: 'Messages',
+        key: '/ui/messages'
+      },
+      {
+        title: 'Tabs',
+        key: '/ui/tabs'
+      },
+      {
+        title: 'Gallery',
+        key: '/ui/gallery'
+      },
+      {
+        title: 'Carousel',
+        key: '/ui/carousel'
+      }
+    ]
+  },
+  {
+    title: 'Form',
+    key: '/form',
+    children: [
+      {
+        title: 'Login',
+        key: '/form/login'
+      },
+      {
+        title: 'Sign up',
+        key: '/form/reg'
+      }
+    ]
+  },
+  {
+    title: 'Table',
+    key: '/table',
+    children: [
+      {
+        title: 'Basic',
+        key: '/table/basic'
+      },
+      {
+        title: 'Advance',
+        key: '/table/high'
+      }
+    ]
+  },
+  {
+    title: 'Text',
+    key: '/rich'
+  },
+  {
+    title: 'City',
+    key: '/city'
+  },
+  {
+    title: 'Order',
+    key: '/order',
+    btnList: [
+      {
+        title: 'Detail',
+        key: 'detail'
+      },
+      {
+        title: 'Finish',
+        key: 'finish'
+      }
+    ]
+  },
+  {
+    title: 'User',
+    key: '/user'
+  },
+  {
+    title: 'Map',
+    key: '/bikeMap'
+  },
+  {
+    title: 'Charts',
+    key: '/charts',
+    children: [
+      {
+        title: 'Bar',
+        key: '/charts/bar'
+      },
+      {
+        title: 'Pie',
+        key: '/charts/pie'
+      },
+      {
+        title: 'Line',
+        key: '/charts/line'
+      }
+    ]
+  },
+  {
+    title: 'Permission',
+    key: '/permission'
+  }
+]
+export default menuList
