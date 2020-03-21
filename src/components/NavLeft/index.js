7f94d127b0770511a34dd163eb66385ced3081f0
David <xiaodi.david@gmail.com>
2020-03-21 22:34:55 +1000

    render nav menu correctly

--- src/components/NavLeft/index.js
+++ src/components/NavLeft/index.js
@@ -4,6 +4,26 @@ import { Menu, Icon } from 'antd'
 import './index.less'
 const SubMenu = Menu.SubMenu
 export default class NavLeft extends React.Component {
+  componentWillMount() {
+    const menuTreeNode = this.renderMenu(MenuConfig)
+    this.setState({ menuTreeNode })
+  }
+  renderMenu = data => {
+    return data.map(item => {
+      if (item.children) {
+        return (
+          <SubMenu title={item.title} key={item.key}>
+            {this.renderMenu(item.children)}
+          </SubMenu>
+        )
+      }
+      return (
+        <Menu.Item title={item.title} key={item.key}>
+          {item.title}
+        </Menu.Item>
+      )
+    })
+  }
   render() {
     return (
       <div>
@@ -11,26 +31,7 @@ export default class NavLeft extends React.Component {
           <img src="/assets/logo-ant.svg" alt=""></img>
           <h1>React MS</h1>
         </div>
-        <Menu theme="dark">
-          <SubMenu
-            key="sub1"
-            title={
-              <span>
-                <Icon type="mail" />
-                <span>Navigation One</span>
-              </span>
-            }
-          >
-            <Menu.ItemGroup title="Item 1">
-              <Menu.Item key="1">Option 1</Menu.Item>
-              <Menu.Item key="2">Option 2</Menu.Item>
-            </Menu.ItemGroup>
-            <Menu.ItemGroup title="Iteom 2">
-              <Menu.Item key="3">Option 3</Menu.Item>
-              <Menu.Item key="4">Option 4</Menu.Item>
-            </Menu.ItemGroup>
-          </SubMenu>
-        </Menu>
+        <Menu theme="dark">{this.state.menuTreeNode}</Menu>
       </div>
     )
   }
