b9c43235816e420c65be26e4933eb5bbb0df25c3
David <xiaodi.david@gmail.com>
2020-03-21 23:42:51 +1000

    header less

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,38 @@
 import React from 'react'
-
+import { Row, Col } from 'antd'
+import './index.less'
+import Util from '../../utils/utils'
 export default class Header extends React.Component {
+  componentWillMount() {
+    this.setState({
+      userName: 'David'
+    })
+    setInterval(() => {
+      let sysTime = Util.formatDate(new Date().getTime())
+      this.setState({
+        sysTime
+      })
+    }, 1000)
+  }
   render() {
-    return <div>Header</div>
+    return (
+      <div className="header">
+        <Row className="header-top">
+          <Col span="24">
+            <span>Welcome, {this.state.userName}</span>
+            <a href="/#">Exit</a>
+          </Col>
+        </Row>
+        <Row className="breadcrumb">
+          <Col className="breadcrumb-title" span="4">
+            Home
+          </Col>
+          <Col className="weather" span="20">
+            <span className="date">{this.state.sysTime}</span>
+            <span className="weather-detail">Storm</span>
+          </Col>
+        </Row>
+      </div>
+    )
   }
 }
