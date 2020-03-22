dcbd70745f9ff79365035b7d89032b2f09c9835c
David <xiaodi.david@gmail.com>
2020-03-22 22:36:35 +1000

    Home component

--- src/admin.js
+++ src/admin.js
@@ -3,20 +3,20 @@ import { Row, Col } from 'antd'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import NavLeft from './components/NavLeft'
+import Home from './pages/home'
 import './style/common.less'
 
 export default class Admin extends React.Component {
   render() {
     return (
       <Row className="container">
-        <Col span="3" className="nav-left">
+        <Col span="4" className="nav-left">
           <NavLeft />
         </Col>
-        <Col span="21" className="main">
+        <Col span="20" className="main">
           <Header />
           <Row className="content">
-            Content
-            {/* {this.props.children} */}
+            <Home />
           </Row>
           <Footer />
         </Col>
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import './index.less'
 
 export default class Footer extends React.Component {
   render() {
-    return <div>Footer</div>
+    return <div className="footer">David | License: MIT</div>
   }
 }
