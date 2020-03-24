b450accb1bf5f48fd400493814272b44e8f494c8
David <xiaodi.david@gmail.com>
2020-03-24 22:17:57 +1000

    UI prompt messages

--- src/router.js
+++ src/router.js
@@ -7,6 +7,7 @@ import Buttons from './pages/ui/buttons'
 import Modals from './pages/ui/modals'
 import Loadings from './pages/ui/loadings'
 import Notifications from './pages/ui/notifications'
+import Messages from './pages/ui/messages'
 // import NoMatch from './pages/nomatch'
 // import { Button } from 'antd'
 
@@ -26,6 +27,7 @@ export default class IRouter extends React.Component {
                   <Route path="/ui/modals" component={Modals} />
                   <Route path="/ui/loadings" component={Loadings} />
                   <Route path="/ui/notifications" component={Notifications} />
+                  <Route path="/ui/messages" component={Messages} />
                 </Switch>
               </Admin>
             )}
