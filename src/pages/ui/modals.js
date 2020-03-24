0e76026b35eb5568dec876558edbd49a6585508f
David <xiaodi.david@gmail.com>
2020-03-24 20:28:32 +1000

    ui modal antD

---
+++ src/pages/ui/modals.js
@@ -0,0 +1,112 @@
+import React from 'react'
+import { Card, Button, Modal } from 'antd'
+import './ui.less'
+export default class Buttons extends React.Component {
+  state = {
+    showModal1: false,
+    showModal2: false,
+    showModal3: false,
+    showModal4: false
+  }
+
+  handleOpen = type => {
+    this.setState({
+      [type]: true
+    })
+  }
+
+  handleConfirm = type => {
+    Modal[type]({
+      title: 'AntD',
+      content: 'Do you want to close this modal?',
+      onOk() {
+        console.log('Ok')
+      },
+      onCancel() {
+        console.log('Cancel')
+      }
+    })
+  }
+  render() {
+    return (
+      <div>
+        <Card title="Modal" className="card-wrap">
+          <Button type="primary" onClick={() => this.handleOpen('showModal1')}>
+            Open
+          </Button>
+          <Button type="primary" onClick={() => this.handleOpen('showModal2')}>
+            Customized
+          </Button>
+          <Button type="primary" onClick={() => this.handleOpen('showModal3')}>
+            20px to Top
+          </Button>
+          <Button type="primary" onClick={() => this.handleOpen('showModal4')}>
+            Vertically Centered
+          </Button>
+        </Card>
+        <Card title="Confirm" className="card-wrap">
+          <Button type="primary" onClick={() => this.handleConfirm('confirm')}>
+            Confirm
+          </Button>
+          <Button type="primary" onClick={() => this.handleConfirm('info')}>
+            Info
+          </Button>
+          <Button type="primary" onClick={() => this.handleConfirm('success')}>
+            Success
+          </Button>
+          <Button type="primary" onClick={() => this.handleConfirm('warning')}>
+            Warning
+          </Button>
+        </Card>
+        <Modal
+          title="React"
+          visible={this.state.showModal1}
+          onCancel={() => {
+            this.setState({
+              showModal1: false
+            })
+          }}
+        >
+          <p>Welcome to AntD CMS</p>
+        </Modal>
+        <Modal
+          title="React"
+          visible={this.state.showModal2}
+          okText="OK"
+          cancelText="Cancel"
+          onCancel={() => {
+            this.setState({
+              showModal2: false
+            })
+          }}
+        >
+          <p>Welcome to AntD CMS</p>
+        </Modal>
+        <Modal
+          title="React"
+          style={{ top: 20 }}
+          visible={this.state.showModal3}
+          onCancel={() => {
+            this.setState({
+              showModal3: false
+            })
+          }}
+        >
+          <p>Welcome to AntD CMS</p>
+        </Modal>
+        <Modal
+          title="React"
+          wrapClassName="vertical-center-modal"
+          visible={this.state.showModal4}
+          onCancel={() => {
+            this.setState({
+              showModal4: false
+            })
+          }}
+        >
+          <p>Welcome to AntD CMS</p>
+        </Modal>
+      </div>
+    )
+  }
+}
--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import App from './App'
 import Login from './pages/login'
 import Admin from './admin'
 import Buttons from './pages/ui/buttons'
+import Modals from './pages/ui/modals'
 // import NoMatch from './pages/nomatch'
 // import { Button } from 'antd'
 
@@ -20,6 +21,7 @@ export default class IRouter extends React.Component {
                 <switch>
                   {/* <Route path='/home' component={Home} /> */}
                   <Route path="/ui/buttons" component={Buttons} />
+                  <Route path="/ui/modals" component={Modals} />
                 </switch>
               </Admin>
             )}
