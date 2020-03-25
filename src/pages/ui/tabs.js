f8e1eb4a55f244060c7723191300e8e88f761005
David <xiaodi.david@gmail.com>
2020-03-25 22:29:13 +1000

    UI TABS

---
+++ src/pages/ui/tabs.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Card, Button, Tabs, message, Icon } from 'antd'
+import './ui.less'
+const TabPane = Tabs.TabPane
+
+export default class Tab extends React.Component {
+  handleCallback = key => {
+    message.info('Hi,您选择了页签：' + key)
+  }
+  render() {
+    return (
+      <div>
+        <Card>
+          <Tabs defaultActiveKey="1" onChange={this.handleCallback}>
+            <TabPane tab="Tab 1" key="1">
+              Content of Tab Pane 1
+            </TabPane>
+            <TabPane tab="Tab 2" key="2">
+              Content of Tab Pane 2
+            </TabPane>
+            <TabPane tab="Tab 3" key="3">
+              Content of Tab Pane 3
+            </TabPane>
+          </Tabs>
+        </Card>
+      </div>
+    )
+  }
+}
