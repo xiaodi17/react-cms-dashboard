f359d052404e2f493557179c5b6a1cf7aa4599bb
David <xiaodi.david@gmail.com>
2020-03-26 00:12:52 +1000

    UI tab edit

--- src/pages/ui/tabs.js
+++ src/pages/ui/tabs.js
@@ -4,9 +4,66 @@ import './ui.less'
 const TabPane = Tabs.TabPane
 
 export default class Tab extends React.Component {
+  newTabIndex = 1
   handleCallback = key => {
     message.info('Hi, this is tab：' + key)
   }
+
+  componentWillMount() {
+    const panes = [
+      {
+        title: 'Tab 1',
+        content: 'Tab 1',
+        key: '1'
+      },
+      {
+        title: 'Tab 2',
+        content: 'Tab 2',
+        key: '2'
+      },
+      {
+        title: 'Tab 3',
+        content: 'Tab 3',
+        key: '3'
+      }
+    ]
+    this.setState({
+      activeKey: panes[0].key,
+      panes
+    })
+  }
+
+  onChange = activeKey => {
+    this.setState({
+      activeKey
+    })
+  }
+
+  onEdit = (targetKey, action) => {
+    this[action](targetKey)
+  }
+
+  add = () => {
+    const panes = this.state.panes
+    const activeKey = `New Tab ${this.newTabIndex++}`
+    panes.push({ title: activeKey, content: 'New Tab Pane', key: activeKey })
+    this.setState({ panes, activeKey })
+  }
+  remove = targetKey => {
+    let activeKey = this.state.activeKey
+    let lastIndex
+    this.state.panes.forEach((pane, i) => {
+      if (pane.key === targetKey) {
+        lastIndex = i - 1
+      }
+    })
+    const panes = this.state.panes.filter(pane => pane.key !== targetKey)
+    if (lastIndex >= 0 && activeKey === targetKey) {
+      activeKey = panes[lastIndex].key
+    }
+    this.setState({ panes, activeKey })
+  }
+
   render() {
     return (
       <div>
@@ -58,6 +115,18 @@ export default class Tab extends React.Component {
             >
               Content of Tab Pane 3
             </TabPane>
+          </Tabs>
+        </Card>
+        <Card title="Tab edit" className="card-wrap">
+          <Tabs
+            onChange={this.onChange}
+            activeKey={this.state.activeKey}
+            type="editable-card"
+            onEdit={this.onEdit}
+          >
+            {this.state.panes.map(panel => {
+              return <TabPane tab={panel.title} key={panel.key} />
+            })}
           </Tabs>
         </Card>
       </div>
